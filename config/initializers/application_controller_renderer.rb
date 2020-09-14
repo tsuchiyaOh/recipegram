169bbcae902ae220621f874b808331655b6db218
tsuchiyaOh <program15977@gmail.com>
2020-09-14 18:05:40 +0900

    complete

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'f13c367a7774d16ad5622fdcbe99b16585750b832bae2a4e257d97724fedaec9335df2f860aecc8189988ca1d09029b7055d8ade5f14b5f20bd6357ac0e7fa63'
